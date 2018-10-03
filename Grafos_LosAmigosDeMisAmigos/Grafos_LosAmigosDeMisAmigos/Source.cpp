3bc1b391f0f5fb72eaba51f333808805113be009
gprossignoli <gprauxiliar@gmail.com>
2018-10-03 09:22:33 +0200

    ejercicio de grafos en desarrollo

---
+++ Grafos_LosAmigosDeMisAmigos/Grafos_LosAmigosDeMisAmigos/Source.cpp
@@ -0,0 +1,59 @@
+﻿// Gerardo Parra Rossignoli
+// TAIS20
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include "Grafo.h"
+
+// función que resuelve el problema
+TipoSolucion resolver(TipoDatos datos) {
+
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+	int persons,friendPairs;
+
+	std::cin >> persons >> friendPairs;
+
+	Grafo city = Grafo(persons);
+	for (int i = 0; i < friendPairs; ++i) {
+		int a, b;
+		std::cin >> a >> b;
+		city.
+	}
+	if(friendPairs > 0)
+	TipoSolucion sol = resolver(datos);
+	// escribir sol
+
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
