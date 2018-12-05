cea2b54123c19a5eb1b3a144723d1b6d6a9eb374
gprossignoli <gprauxiliar@gmail.com>
2018-12-05 10:46:09 +0100

    ej 60 en desarrollo

--- ProgramacionDinamica_Varillas/Source.cpp
+++ ProgramacionDinamica_Varillas/Source.cpp
@@ -13,31 +13,29 @@ typedef struct {
 	int cost;
 }tRod;
 
-// función que resuelve el problema
-bool getRodIsPossible(std::vector<tRod> const& rods,const int L) {
-	Matriz<int> m(rods.size(), L + 1,0);
+/*
+	posible(0,l) = false, l > 0
+	posible(i,0) = true, i >= 0	
+	posible(i,l) = { posible(i-1, l) si l[i] > l		 
+				    posible(i-1,l-l[i]) || posible(i-1,l) }
 
-	//coste = N^2, siendo N el numero de varillas
-	for (int i = 0; i < rods.size(); ++i) {
-		int lenght = 0;
-		for (int r = i; r >= 0; --r) {
-			lenght += rods[r].lenght;
-			if (lenght <= L)
-				m[i][lenght] = 1;
+*/
+
+bool isPossible(std::vector<tRod> const& rods, int L) {
+	std::vector<std::vector<bool>> m(rods.size(), std::vector<bool>(L + 1, false));
+	m[0][0] = true;
+	for (int i = 1; i < rods.size(); ++i) {
+		m[i][0] = true;
+		for (int l = 1; l <= L; ++l) {
+			if (rods[i].lenght <= l) m[i][l] = (m[i - 1][l - rods[i].lenght] || m[i - 1][l]);
+			else m[i][l] = m[i - 1][l];
 		}
 	}
-	
-	bool possible = false;
-	int i = 0;
-	//coste = N, siendo N el numero de varillas
-	while (i < m.numfils() && !possible) {
-		if (m[i][L] == 1)
-			possible = true;
-		++i;
-	}
 
-	return possible;
+
+	return m[rods.size()-1][L];
 }
+
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
@@ -47,12 +45,16 @@ bool resuelveCaso() {
 	if (!std::cin)
 		return false;
 
-	std::vector<tRod> rods(N);
-	for (int i = 0; i < N; ++i) {
+	std::vector<tRod> rods(N+1);
+	rods[0] = { 0,0 };
+	for (int i = 1; i <= N; ++i) {
 		std::cin >> rods[i].lenght >> rods[i].cost;
 	}
 
-	std::cout << getRodIsPossible(rods, L) << " ";
+	if (isPossible(rods, L))
+		std::cout << "SI ";
+	else
+		std::cout << "NO ";
 
 	return true;
 
